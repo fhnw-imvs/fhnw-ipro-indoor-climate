112dc5dd8bbba9af60d158e4f55b2c2f7972bbd5
tamberg <tamberg@yaler.net>
2025-08-24 16:34:45 +0200

    Update.

--- level-1/Python/serial_read/serial_read.py
+++ level-1/Python/serial_read/serial_read.py
@@ -1,6 +1,6 @@
 import serial
 
-port = serial.Serial('/dev/tty.usbmodem102') # or /dev/tty.u...
+port = serial.Serial('/dev/tty.usbmodem102') # or 'COM3'
 port.baudrate = 115200
 bytes = port.read()
 print(bytes)
