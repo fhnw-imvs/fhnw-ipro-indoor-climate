50d2d197db67d371a5274143b0d2dc09b6dfccc1
tamberg <tamberg@yaler.net>
2025-08-29 15:44:47 +0200

    Add.

---
+++ level-1/Python/csv_write/csv_write.py
@@ -0,0 +1,15 @@
+// License https://creativecommons.org/publicdomain/zero/1.0
+// based on ../../../prompts/duck.ai_2025-08-29_11-41-09.txt
+
+import csv
+
+data = [
+    ["City", "ZIP Code"],
+    ["Bern", 3000],
+    ["Basel", 4000],
+    ["Zürich", 8000],
+]
+
+with open("data.csv", mode='w', newline='\n') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
