d26db2a3c7fd1a69978f28490479f24eaffbbac8
tamberg <tamberg@yaler.net>
2025-08-31 17:19:20 +0200

    Update.

--- level-1/Java/csv_write/src/main/java/Program.java
+++ level-1/Java/csv_write/src/main/java/Program.java
@@ -1,5 +1,5 @@
 // License https://creativecommons.org/publicdomain/zero/1.0
-// based on ../../../prompts/duck.ai_2025-08-29_11-45-00.txt
+// based on $REPO_PATH/prompts/duck.ai_20250829T094500Z.txt #2
 
 import java.io.FileWriter;
 import java.io.IOException;
