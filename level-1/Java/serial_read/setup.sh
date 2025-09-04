75b653363ed74f08a190900c3aa742688a240f35
tamberg <tamberg@yaler.net>
2025-09-04 16:48:34 +0200

    Update.

--- level-1/Java/serial_read/setup.sh
+++ level-1/Java/serial_read/setup.sh
@@ -2,3 +2,4 @@
 mkdir target
 mkdir lib
 #curl -Lo lib/x.jar https://github.com/user/project/x.jar
+curl -Lo lib/nrjavaserial-5.2.1.jar https://github.com/NeuronRobotics/nrjavaserial/releases/download/5.2.1/nrjavaserial-5.2.1.jar
