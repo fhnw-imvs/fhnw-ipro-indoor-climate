fee43955b8c155801f3657034105e163f3d30807
tamberg <tamberg@yaler.net>
2025-09-04 16:40:43 +0200

    Update.

--- level-1/Java/csv_write/src/main/java/Program.java
+++ level-1/Java/csv_write/src/main/java/Program.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public final class Program {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         String[][] data = {
             {"City", "ZIP Code"},
             {"Bern", "3000"},
