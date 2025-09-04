518c2b085f91cd3bdb81b52723232c53b27bad44
tamberg <tamberg@yaler.net>
2025-09-04 16:49:08 +0200

    Update.

--- level-1/Java/serial_read/src/main/java/Program.java
+++ level-1/Java/serial_read/src/main/java/Program.java
@@ -1,5 +1,27 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import gnu.io.NRSerialPort;
+
 public final class Program {
-    public static void main(String args[]) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) {
+        String port = "/dev/tty.u..."; // TODO
+
+        int baudRate = 115200;
+        NRSerialPort serial = new NRSerialPort(port, baudRate);
+        serial.connect();
+
+        DataInputStream ins = new DataInputStream(serial.getInputStream());
+        try {
+	        while (!Thread.interrupted()) {
+		        if (ins.available() > 0) {
+			        int b = ins.read();
+			        System.out.print((char) b);
+                }
+                Thread.sleep(5);
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        serial.disconnect();
     }
 }
