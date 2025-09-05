2b2b0bb8ace6ac9693da8b2d822e473cd7a3e33a
tamberg <tamberg@yaler.net>
2025-09-05 14:06:44 +0200

    Update.

--- level-1/Java/serial_read/src/main/java/Program.java
+++ level-1/Java/serial_read/src/main/java/Program.java
@@ -1,26 +1,35 @@
-import java.io.DataInputStream;
-import gnu.io.NRSerialPort;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import com.fazecast.jSerialComm.SerialPortInvalidPortException;
+import com.fazecast.jSerialComm.SerialPort;
 
 public final class Program {
     public static void main(String[] args) {
-        String port = "/dev/tty.usbmodem102"; // or "COM3", TODO
-
-        int baudRate = 115200;
-        NRSerialPort serial = new NRSerialPort(port, baudRate);
-        serial.connect();
-
-        DataInputStream ins = new DataInputStream(serial.getInputStream());
+        String name = "/dev/tty.usbmodem112302";
+        SerialPort p = null;
+        boolean open = false;
         try {
-	        while (!Thread.interrupted()) {
-		        if (ins.available() > 0) {
-			        int b = ins.read();
-			        System.out.print((char) b);
+            p = SerialPort.getCommPort(name);
+            p.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 0);
+            p.setBaudRate(115200);
+            open = p.openPort();
+        } catch (SerialPortInvalidPortException e) {}
+        if (p != null && open) {
+            try (InputStream is = p.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr)) {
+                while (true) {
+                    String line = br.readLine();
+                    System.out.println(line);
                 }
-                Thread.sleep(5);
+            } catch (IOException e){
+                e.printStackTrace();
             }
-        } catch (Exception ex){
-            ex.printStackTrace();
+        } else {
+            System.out.println("Cannot open port.");
         }
-        serial.disconnect();
     }
 }
