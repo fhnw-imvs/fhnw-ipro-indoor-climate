45866a208063e38f21f795aca62d3a2a43843930
tamberg <tamberg@yaler.net>
2025-09-05 14:11:42 +0200

    Update.

--- level-1/Java/serial_read/src/main/java/Program.java
+++ level-1/Java/serial_read/src/main/java/Program.java
@@ -3,33 +3,24 @@ import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import com.fazecast.jSerialComm.SerialPortInvalidPortException;
 import com.fazecast.jSerialComm.SerialPort;
 
 public final class Program {
     public static void main(String[] args) {
         String name = "/dev/tty.usbmodem112302";
-        SerialPort p = null;
-        boolean open = false;
-        try {
-            p = SerialPort.getCommPort(name);
-            p.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 0);
-            p.setBaudRate(115200);
-            open = p.openPort();
-        } catch (SerialPortInvalidPortException e) {}
-        if (p != null && open) {
-            try (InputStream is = p.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr)) {
-                while (true) {
-                    String line = br.readLine();
-                    System.out.println(line);
-                }
-            } catch (IOException e){
-                e.printStackTrace();
+        SerialPort p = SerialPort.getCommPort(name);
+        p.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 0);
+        p.setBaudRate(115200);
+        p.openPort();
+        try (InputStream is = p.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr)) {
+            while (true) {
+                String line = br.readLine();
+                System.out.println(line);
             }
-        } else {
-            System.out.println("Cannot open port.");
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
