e5ac8ffa6d783d25cd5ad13432ca2d02a94054a2
tamberg <tamberg@yaler.net>
2025-09-05 14:17:43 +0200

    Update.

--- level-1/Java/serial_read/src/main/java/Program.java
+++ level-1/Java/serial_read/src/main/java/Program.java
@@ -1,13 +1,13 @@
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 import com.fazecast.jSerialComm.SerialPort;
 
 public final class Program {
     public static void main(String[] args) {
-        String name = "/dev/tty.usbmodem102"; // or "COM3"
+        String name = "/dev/tty.u..."; // or "COM3"
         SerialPort p = SerialPort.getCommPort(name);
         p.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 0);
         p.setBaudRate(115200);
